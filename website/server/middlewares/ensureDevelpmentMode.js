9cba9da2eef46bb6eedafe5eca9984eeacdf2607
Sabe Jones <sabe@habitica.com>
2024-02-09 14:20:30 -0600

    chore(testing): enable "develpment" mode

--- website/server/middlewares/ensureDevelpmentMode.js
+++ website/server/middlewares/ensureDevelpmentMode.js
@@ -1,12 +1,3 @@
-import nconf from 'nconf';
-import {
-  NotFound,
-} from '../libs/errors';
-
 export default function ensureDevelpmentMode (req, res, next) {
-  if (nconf.get('IS_PROD')) {
-    next(new NotFound());
-  } else {
-    next();
-  }
+  next();
 }
